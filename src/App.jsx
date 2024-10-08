c15f0d2e7605c427fc43a1cc85b1a5dfab150d2c
aditya90205 <adityasrivastav90205@gmail.com>
2024-10-08 20:40:45 +0530

    Update package dependencies and add react-toastify library

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,74 @@
-import './App.css'
-import Form from './components/Form'
+// import './App.css'
+// import Form from './components/Form'
+// import img from './assets/images/img.jpg';
+// import logo from './assets/images/logo.png';
+// import FranchiseeSection from './components/FranchiseeSection';
+// import Choose from './components/Choose';
+// import FAQComponent from './components/FAQComponent';
+
+// function App() {
+ 
+
+//   return (
+//     <>
+//     <div className=''>
+//       <img src={logo} alt="logoImg" className='mx-auto'/>
+//       <div className='flex flex-col md:flex-row items-center justify-center'>
+//         <img src={img} alt="image" className='w-3/4 md:w-1/2'/>
+//       <Form />  
+//       </div>
+//     </div>
+//     <FranchiseeSection />
+//     <Choose />
+//     <FAQComponent />
+//     </>
+//   )
+// }
+
+// export default App
+
+import './App.css';
+import Form from './components/Form';
 import img from './assets/images/img.jpg';
 import logo from './assets/images/logo.png';
 import FranchiseeSection from './components/FranchiseeSection';
 import Choose from './components/Choose';
 import FAQComponent from './components/FAQComponent';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Import the toastify CSS
 
 function App() {
- 
-
   return (
     <>
-    <div className=''>
-      <img src={logo} alt="logoImg" className='mx-auto'/>
-      <div className='flex flex-col md:flex-row items-center justify-center'>
-        <img src={img} alt="image" className='w-3/4 md:w-1/2'/>
-      <Form />  
+      {/* Main content */}
+      <div className=''>
+        <img src={logo} alt="logoImg" className='mx-auto' />
+        <div className='flex flex-col md:flex-row items-center justify-center'>
+          <img src={img} alt="image" className='w-3/4 md:w-1/2' />
+          <Form />
+        </div>
       </div>
-    </div>
-    <FranchiseeSection />
-    <Choose />
-    <FAQComponent />
+      
+      {/* Other components */}
+      <FranchiseeSection />
+      <Choose />
+      <FAQComponent />
+
+      {/* Toast Container for global use */}
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
